5a81956ba67a0c4ac557d699134f761b42225856
Alexander Tetenkov <forthe@ya.ru>
2022-03-03 20:00:18 +0300

    print sa

--- subs_secrets.py
+++ subs_secrets.py
@@ -9,7 +9,7 @@ def substitude_serv_acc():
     with open('serv_acc.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
 
-    data['private_key'] = private_key.replace(' ', '\n')
+    data['private_key'] = private_key
     data['private_key_id'] = private_key_id
     print(data)
     with open('serv_acc.json', 'w', encoding='utf-8') as file:
