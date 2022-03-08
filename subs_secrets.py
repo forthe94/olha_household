5634be4c80e5a63b9da4df1cf9b5fd5a22fa4313
Alexander Tetenkov <forthe@ya.ru>
2022-03-08 15:49:13 +0300

    full acc in env

--- subs_secrets.py
+++ subs_secrets.py
@@ -3,15 +3,10 @@ import os
 
 private_key = os.getenv('PRIVATE_KEY')
 private_key_id = os.getenv('PRIVATE_KEY_ID')
+serv_acc = os.getenv('SERV_ACC')
 
 
 def substitude_serv_acc():
-    with open('serv_acc.json', 'r', encoding='utf-8') as file:
-        data = json.load(file)
-
-    data['private_key'] = private_key
-    data['private_key_id'] = private_key_id
-    print(data)
     with open('serv_acc.json', 'w', encoding='utf-8') as file:
-        json.dump(data, file)
+        file.write(serv_acc)
 
